d3a6cb2a96bf7b342810d45242717ac23dd7cc0c
EfrenSango <efren.sanchezgon@gmail.com>
2019-05-20 17:47:23 +0200

    Updates

---
+++ precache-manifest.bcfb7d935031c12b21e9955ae21fbedb.js
@@ -0,0 +1,29 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa31bca6a7d3e9b6c0d8",
+    "url": "/recetas/static/css/main.6b533f4a.chunk.css"
+  },
+  {
+    "revision": "aa31bca6a7d3e9b6c0d8",
+    "url": "/recetas/static/js/main.aa31bca6.chunk.js"
+  },
+  {
+    "revision": "33b881ce09beef2caa46",
+    "url": "/recetas/static/js/runtime~main.33b881ce.js"
+  },
+  {
+    "revision": "b401314f6f68976b44ef",
+    "url": "/recetas/static/js/2.b401314f.chunk.js"
+  },
+  {
+    "revision": "67121ff22687677622bf9f13578024ce",
+    "url": "/recetas/index.html"
+  },
+  {
+    "url": "/recetas/precache-manifest.ec5c94049562871655b8dfdbd86b089d.js"
+  },
+  {
+    "revision": "3f66c63403e7f440c21e0cbcd8c8e8aa",
+    "url": "/recetas/service-worker.js"
+  }
+];
---
+++ precache-manifest.ec5c94049562871655b8dfdbd86b089d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa31bca6a7d3e9b6c0d8",
+    "url": "/recetas/static/css/main.6b533f4a.chunk.css"
+  },
+  {
+    "revision": "aa31bca6a7d3e9b6c0d8",
+    "url": "/recetas/static/js/main.aa31bca6.chunk.js"
+  },
+  {
+    "revision": "33b881ce09beef2caa46",
+    "url": "/recetas/static/js/runtime~main.33b881ce.js"
+  },
+  {
+    "revision": "b401314f6f68976b44ef",
+    "url": "/recetas/static/js/2.b401314f.chunk.js"
+  },
+  {
+    "revision": "67121ff22687677622bf9f13578024ce",
+    "url": "/recetas/index.html"
+  }
+];
